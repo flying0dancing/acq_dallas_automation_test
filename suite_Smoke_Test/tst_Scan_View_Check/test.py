304963c72e173a9efc694be7fbcbdddc0f38a4e4
Eva <lingfeng.huang@csdental.com>
2019-08-08 13:05:30 +0800

    Add checkButton and OpenWithButton png

--- suite_Smoke_Test/tst_Scan_View_Check/test.py
+++
@@ -1,7 +0,0 @@
-APPNAME = "cs_scanflow C:\\ProgramData\\TW\\AcqAltair\\InputData.xml"
-
-def main():
-    source(findFile("scripts", "common.py"))
-    startApplication(APPNAME)
-    
-    scanViewButtonsCheck()
