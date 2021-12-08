c8afd3dbe09c7c436343b10835756f820f3394ff
Eva <lingfeng.huang@csdental.com>
2021-12-08 17:16:09 +0800

    check what's new, and then recover data and then userlogin

--- suite_automation_test/tst_00_scan_view/test.py
+++ suite_automation_test/tst_00_scan_view/test.py
@@ -9,15 +9,17 @@ def main():
     #Click ok button on the warn message which says it's an internal version
     skipInternalVersionDlg()
     
-    recoverDataDlg()#recover dialog is in front of sign in dialog
-    
-    userLogin("andy.qin@carestream.com","kdis7")
-        
     #Click buttons in "What's new" page
     mouseClick(UiTypes.ScreenPoint(410,920), Qt.NoModifier, Qt.LeftButton)
     snooze(2)
     mouseClick(UiTypes.ScreenPoint(960, 920), Qt.NoModifier, Qt.LeftButton)
     snooze(3)
+    
+    recoverDataDlg()#recover dialog is in front of sign in dialog
+    
+    userLogin("andy.qin@carestream.com","kdis7")
+        
+    
     
 
     scanViewButtonsCheck()
